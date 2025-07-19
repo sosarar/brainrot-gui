aeebf278e8d8329ea8a24f2c4c1ecc03d791dffc
sosarar <btcpry@gmail.com>
2025-07-18 20:10:50 -0500

    Create gui.lua

---
+++ gui.lua
@@ -0,0 +1,20 @@
+-- gui.lua
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
+
+local Window = OrionLib:MakeWindow({Name = "Brainrot Hub", HidePremium = false, SaveConfig = false})
+
+local Tab = Window:MakeTab({Name = "Main", Icon = "rbxassetid://4483345998", PremiumOnly = false})
+
+Tab:AddButton({
+	Name = "Auto Steal Brainrots",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/sosarar/brainrot-gui/main/autosteal.lua"))()
+	end
+})
+
+Tab:AddButton({
+	Name = "ESP Brainrots",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/sosarar/brainrot-gui/main/esp.lua"))()
+	end
+})
